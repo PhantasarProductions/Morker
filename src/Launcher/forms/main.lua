cfbb6bb78f949952d2b35020bf23b0fe29113d53
Jeroen Broks <facebook@tbbs.nl>
2017-03-01 22:45:47 +0100

    Test functions. The actual content to these functions will be filled out later, since they require the support of features Maan currently doesn't have

--- src/Launcher/forms/main.lua
+++ src/Launcher/forms/main.lua
@@ -2,3 +2,10 @@ function FORM_main_Close(key)
     os.exit()
 end
 
+function KID_BUTTON_NewGame_Action(key)
+   alert("Test - New Game: "..key)
+end   
+
+function KID_BUTTON_LoadGame_Action(key)
+   alert("Test - Load Game: "..key)
+end   
