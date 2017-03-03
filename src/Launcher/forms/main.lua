ad624ee70e9eb746779b169285db3d905eb1e106
Jeroen Broks <facebook@tbbs.nl>
2017-03-03 19:15:07 +0100

    Netwerk settings (GJ & Anna) can be saved now

--- src/Launcher/forms/main.lua
+++ src/Launcher/forms/main.lua
@@ -1,7 +1,8 @@
 Netwerk = Netwerk or {Anna={}, GameJolt={}}
 
 function FORM_main_Close(key)
-    os.exit()
+    --os.exit()
+    Bye()
 end
 
 function KID_BUTTON_NewGame_Action(key)
@@ -18,3 +19,16 @@ function KID_BUTTON_NET_Anna_Create_Action(key)
    if AnnaName=="" then return end
    alert("Very well, "..AnnaName..";\n\nI will now try to create your account. If succesful you'll be led to the website for further instructions.")
 end      
+
+function KID_TEXTFIELD_NET_Anna_ID_Action(key)        Netwerk.Anna.ID        = MAAN_Text('KID_TEXTFIELD_NET_Anna_ID')        end
+function KID_TEXTFIELD_NET_Anna_Secu_Action(key)      Netwerk.Anna.Secu      = MAAN_Text('KID_TEXTFIELD_NET_Anna_Secu')      end
+function KID_TEXTFIELD_NET_GameJolt_User_Action(key)  Netwerk.GameJolt.User  = MAAN_Text('KID_TEXTFIELD_NET_GameJolt_User')  end
+function KID_TEXTFIELD_NET_GameJolt_Token_Action(key) Netwerk.GameJolt.Token = MAAN_Text('KID_TEXTFIELD_NET_GameJolt_Token') end
+
+function GALE_OnLoad()
+  CSay(serialize("Netwerk",Netwerk))
+  MAAN_Text('KID_TEXTFIELD_NET_Anna_ID',       Netwerk.Anna.ID) 
+  MAAN_Text('KID_TEXTFIELD_NET_Anna_Secu',     Netwerk.Anna.Secu) 
+  MAAN_Text('KID_TEXTFIELD_NET_GameJolt_User', Netwerk.GameJolt.User)
+  MAAN_Text('KID_TEXTFIELD_NET_GameJolt_Token',Netwerk.GameJolt.Token)
+end
