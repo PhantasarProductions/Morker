fd99088cc5ef900ed1335f253a334c4f5f24759e
Tricky1975 <facebook@tbbs.nl>
2017-08-17 14:06:48 +0200

    Base

--- Script/ILoveYou.lua
+++ Script/ILoveYou.lua
@@ -1,1 +1,62 @@
--- Content comes later!
+--[[
+**********************************************
+  
+  ILoveYou.lua
+  (c) Jeroen Broks, 2017, All Rights Reserved.
+  
+  This file contains material that is related 
+  to a storyline that is which is strictly
+  copyrighted to Jeroen Broks.
+  
+  This file may only be used in an unmodified
+  form with an unmodified version of the 
+  software this file belongs to.
+  
+  You may use this file for your study to see
+  how I solved certain things in the creation
+  of this project to see if you find valuable
+  leads for the creation of your own.
+  
+  Mostly this file comes along with a project
+  that is for most part released under an
+  open source license and that means that if
+  you use that code with this file removed
+  from it, you can use it under that license.
+  Please check out the other files to find out
+  which license applies.
+  This file comes 'as-is' and in no possible
+  way the author can be held responsible of
+  any form of damages that may occur due to 
+  the usage of this file
+  
+  
+ **********************************************
+ 
+version: 17.08.17
+]]
+
+print("Morker - By Jeroen Broks")
+
+-- *import chain_lunamorica
+
+mkl.version("Morker - ILoveYou.lua","17.08.17")
+mkl.lic    ("Morker - ILoveYou.lua","Phantasar Closed License")
+
+
+function saveconfig()
+   local saveconfig = serialize('local config',config).."\n\nreturn config\n\n"
+   love.filesystem.write('config/config.lua',saveconfig)
+end
+
+function love.load()
+   love.filesystem.createDirectory("config")
+   love.filesystem.createDirectory("savegames")
+   love.filesystem.createDirectory("swap")   
+   if love.filesystem.isFile("config/config.lua") then config = j_love_import('config/config.lua') end
+   config = config or { screen='full'}
+   saveconfig() 
+end
+
+function love.quit()
+   saveconfig()
+end   
